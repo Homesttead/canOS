a66291743ba951d4052f4beb55eb97766d054a5a
Homesttead <152499403+Homesttead@users.noreply.github.com>
2024-03-06 18:14:53 +0200

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -3,3 +3,5 @@ shell.run("cd /canOS")
 shell.run("wget https://raw.githubusercontent.com/Homesttead/canOS/main/canOS/canOS.lua canOS.lua")
 shell.run("cd /")
 shell.run("wget https://raw.githubusercontent.com/Homesttead/canOS/main/canOS/startup.lua startup.lua")
+
+print("finished installation")
